415d9ced09f6f9d0f26f225a2953f724afd53297
aabanakhtar <aabanakhtar.home@gmail.com>
2025-06-03 18:41:06 -0400

    initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/Teleop.kt
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode.opmode
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+
+@TeleOp(name = "TeleOp")
+class Teleop : OpMode()
+{
+
+
+    override fun init()
+    {
+
+    }
+
+    override fun loop()
+    {
+    }
+
+}
